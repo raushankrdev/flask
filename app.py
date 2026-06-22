095d814612722bd91882e3967780dde935096536
raushankrdev <raushankumar.connect@gmail.com>
2026-06-22 19:29:43 +0530

    readme file updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
 #SQLite configuration
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
